bcf71b29191ee5c6bccd9c0c4faa3ddb3ce56911
C043 <mariofragnitoph@gmail.com>
2024-07-05 16:23:42 +0200

    Removed suncalc

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,16 @@ import Home from "./pages/Home";
 import Location from "./pages/Location";
 import Welcome from "./components/Welcome";
 import { useState } from "react";
+import { getTimes, SunCalc } from "suncalc";
 
 function App() {
   const [hours, setTime] = useState("");
   const [theme, setTheme] = useState("day");
+  const getSunset = () => {
+    const today = SunCalc.getTimes(new Date());
+    const sunRiseHour = today.sunrise.getHours() + ":" + today.sunrise.getMinutes();
+    console.log(sunRiseHour);
+  };
   const getTime = () => {
     setInterval(() => {
       const today = new Date();
@@ -18,6 +24,7 @@ function App() {
   };
   useState(() => {
     getTime();
+    getSunset();
   }, []);
 
   return (
